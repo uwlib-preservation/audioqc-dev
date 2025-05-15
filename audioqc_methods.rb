32ddfb10a548e195c03daeb43d06cddf126827da
Andrew Weaver <theandrewjw@gmail.com>
2025-05-15 10:01:33 -0700

    output overall volume max

--- audioqc_methods.rb
+++ audioqc_methods.rb
@@ -165,12 +165,12 @@ class QcTarget
 
   def write_csv_line(output_csv)
     if ! File.exist?(output_csv)
-      header = ['Path', 'Warnings', 'Channels', 'Duration', 'Channel 1 max', 'Channel 2 max', 'Average Phase', 'MD5 check', 'Mediaconch Status', 'Mediaconch Failures', 'Coding History']
+      header = ['Path', 'Warnings', 'Channels', 'Duration', 'Volume max', 'Channel 1 max', 'Channel 2 max', 'Average Phase', 'MD5 check', 'Mediaconch Status', 'Mediaconch Failures', 'Coding History']
       CSV.open(output_csv, 'a') do |csv|
         csv << header
       end
     end
-    line = [@input_path,@warnings.flatten.join(', '),@channel_count, @duration_normalized, @channel_one_max,@channel_two_max,@average_phase,@md5_alert, @conch_result, @conch_failures.flatten.join(', '),@coding_history]
+    line = [@input_path,@warnings.flatten.join(', '),@channel_count, @duration_normalized, @overall_volume_max, @channel_one_max,@channel_two_max,@average_phase,@md5_alert, @conch_result, @conch_failures.flatten.join(', '),@coding_history]
     CSV.open(output_csv, 'a') do |csv|
       csv << line
     end
